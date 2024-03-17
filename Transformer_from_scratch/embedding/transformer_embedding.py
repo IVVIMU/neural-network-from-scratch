c6f7d0617956191b56d5aaa7f18b77dd264c3d71
Wood.FY <zfyfishwood@gmail.com>
2024-03-17 22:06:30 +0800

    Add Transformer Embedding

---
+++ Transformer_from_scratch/embedding/transformer_embedding.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+from token_embedding import TokenEmbedding
+from positional_encoding import PositionalEncoding
+
+
+class TransformerEmbedding(nn.Module):
+    """
+    token embedding + positional encoding
+    positional encoding can give positional information to network
+    """
+
+    def __init__(self, vocab_size, d_model, max_seq_len, dropout, device):
+        """
+        class for word embedding that included positional information
+        :param vocab_size: size of vocabulary
+        :param d_model: dimension of model
+        :param max_seq_len: max sequence length
+        :param dropout: rate of dropout
+        :param device: hardware device setting
+        """
+        super(TransformerEmbedding, self).__init__()
+
+        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        self.pos_emb = PositionalEncoding(d_model, max_seq_len, device)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        tok_emb = self.tok_emb(x)
+        pos_emb = self.pos_emb(x)
+        out = self.dropout(tok_emb + pos_emb)
+        return out
