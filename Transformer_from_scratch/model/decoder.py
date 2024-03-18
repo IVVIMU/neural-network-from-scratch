1a917ccf0088e12e5e4d5ebdcba989a662a9cf03
Wood.FY <zfyfishwood@gmail.com>
2024-03-18 16:43:12 +0800

    Add Decoder

---
+++ Transformer_from_scratch/model/decoder.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+
+from embedding.transformer_embedding import TransformerEmbedding
+from blocks.decoder_block import DecoderBlock
+
+
+class Decoder(nn.Module):
+
+    def __init__(
+            self,
+            trg_vocab_size,
+            max_seq_len,
+            d_model,
+            ffn_hidden,
+            n_heads,
+            n_layers,
+            dropout,
+            device
+    ):
+        super().__init__()
+
+        self.emb = TransformerEmbedding(
+            vocab_size=trg_vocab_size,
+            max_seq_len=max_seq_len,
+            d_model=d_model,
+            dropout=dropout,
+            device=device
+        )
+
+        self.layers = nn.ModuleList(
+            [
+                DecoderBlock(
+                    d_model=d_model,
+                    ffn_hidden=ffn_hidden,
+                    n_heads=n_heads,
+                    dropout=dropout
+                )
+                for _ in range(n_layers)
+            ]
+        )
+
+        self.linear = nn.Linear(d_model, trg_vocab_size)
+
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+        trg = self.emb(trg)
+
+        for layer in self.layers:
+            trg = layer(trg, enc_src, trg_mask, src_mask)
+
+        output = self.linear(trg)
+
+        return output
