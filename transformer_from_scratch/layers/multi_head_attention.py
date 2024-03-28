83fbcb50608761e8d2f8581b9c9a60187fbde6ee
Wood.FY <zfyfishwood@gmail.com>
2024-03-28 23:11:13 +0800

    Update attention

--- transformer_from_scratch/layers/multi_head_attention.py
+++ transformer_from_scratch/layers/multi_head_attention.py
@@ -6,59 +6,87 @@ from transformer_from_scratch.layers.scaled_dot_product_attention import ScaledD
 class MultiHeadAttention(nn.Module):
 
     def __init__(self, d_model, n_heads):
+        """
+        Args:
+            d_model: dimension of embeddings
+            n_heads: number of self attention heads
+        """
         super(MultiHeadAttention, self).__init__()
 
+        assert d_model % n_heads == 0
+        self.d_model = d_model
         self.n_heads = n_heads
+        self.d_head = d_model // n_heads
+
         self.attention = ScaledDotProductAttention()
-        self.w_q = nn.Linear(d_model, d_model)
-        self.w_k = nn.Linear(d_model, d_model)
-        self.w_v = nn.Linear(d_model, d_model)
-        self.w_concat = nn.Linear(d_model, d_model)
 
-    def split(self, tensor):
-        """
-        split tensor by number of heads
+        self.w_q = nn.Linear(d_model, d_model)  # query weights
+        self.w_k = nn.Linear(d_model, d_model)  # key weights
+        self.w_v = nn.Linear(d_model, d_model)  # value weights
+        self.w_o = nn.Linear(d_model, d_model)  # output weights
 
-        :param tensor: [batch_size, seq_len, d_model]
-        :return: [batch_size, seq_len, n_heads, d_head]
-        """
-        batch_size, seq_len, d_model = tensor.size()
+    # def split(self, tensor):
+    #     """
+    #     Args:
+    #         tensor: (batch_size, seq_len, d_model)
 
-        d_head = d_model // self.n_heads
-        # [batch_size, seq_len, n_heads, d_head] -> [batch_size, n_heads, seq_len, d_head]
-        tensor = tensor.view(batch_size, seq_len, self.n_heads, d_head).transpose(1, 2)
+    #     Returns: 
+    #         tensor: (batch_size, seq_len, n_heads, d_head]
+    #     """
+    #     batch_size, seq_len, d_model = tensor.size()
 
-        return tensor
+    #     # [batch_size, seq_len, n_heads, d_head] -> [batch_size, n_heads, seq_len, d_head]
+    #     tensor = tensor.view(batch_size, seq_len, self.n_heads, self.d_head).transpose(1, 2)
 
-    def concat(self, tensor):
-        """
-        inverse function of self.split(tensor : torch.Tensor)
+    #     return tensor
 
-        :param tensor: [batch_size, n_heads, seq_len, d_head]
-        :return: [batch_size, seq_len, d_model]
-        """
-        batch_size, n_heads, seq_len, d_head = tensor.size()
-        # make the tensor contiguous in memory
-        tensor = tensor.transpose(1, 2).contiguous().view(batch_size, seq_len, n_heads * d_head)
+    # # def concat(self, tensor):
+    # #     """
+    # #     inverse function of self.split(tensor : torch.Tensor)
 
-        return tensor
+    # #     :param tensor: [batch_size, n_heads, seq_len, d_head]
+    # #     :return: [batch_size, seq_len, d_model]
+    # #     """
+    # #     batch_size, n_heads, seq_len, d_head = tensor.size()
+    # #     # make the tensor contiguous in memory
+    # #     tensor = tensor.transpose(1, 2).contiguous().view(batch_size, seq_len, n_heads * d_head)
+
+    # #     return tensor
 
     def forward(self, q, k, v, mask=None):
-        # 1. dot product with weight matrix
-        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+        """
+        Args:
+            q: query vector (batch_size, q_len, d_model)
+            k: key vector (batch_size, k_len, d_model)
+            v: value vector (batch_size, v_len, d_model)
+            mask: mask for decoder
 
-        # 2. split tensor by number of heads
-        # [batch_size, seq_len, n_heads, d_head]
-        q, k, v = self.split(q), self.split(k), self.split(v)
+        Returns:
+            output: attention values (batch_size, q_len, d_model)
+            attention_probs: softmax scores (batch_size, n_heads, q_len, k_len)
+        """
+        batch_size = k.size(0)
 
-        # 3. do scaled dot-product to compute similarity
-        out, attention = self.attention(q, k, v, mask=mask)
+        # 1. calculate query, key and value tensors
+        # (batch_size, seq_length, d_model) x (d_model, d_model) = (batch_size, seq_length, d_model)
+        Q, K, V = self.w_q(q), self.w_k(k), self.w_v(v)
 
-        # 4. concat and pass to linear layer
-        out = self.concat(out)
-        out = self.w_concat(out)
+        # 2. split each tensor into n-heads to compute attention
+        # (batch_size, seq_len, n_heads, d_head) -> (batch_size, n_heads, seq_len, d_head)
+        Q = Q.view(batch_size, -1, self.n_heads, self.d_head).permute(0, 2, 1, 3)
+        K = K.view(batch_size, -1, self.n_heads, self.d_head).permute(0, 2, 1, 3)
+        V = V.view(batch_size, -1, self.n_heads, self.d_head).permute(0, 2, 1, 3)
+
+        # 3. do scaled dot-product (QK^{T}) to compute similarity
+        attention, attention_probs = self.attention(Q, K, V, mask=mask)
+
+        # 4. reshape attention back to (batch_size, seq_len, d_model)
+        # (batch_size, n_heads, seq_len, d_head) -> (batch_size, seq_len, n_heads, d_head) -> (batch_size, seq_len, d_model)
+        attention = attention.permute(0, 2, 1, 3).contiguous().view(batch_size, -1, self.n_heads * self.d_head)
 
         # 5. visualize attention map
         # TODO: implement visualization
 
-        return out
+        out = self.w_o(attention)
+
+        return out, attention_probs
--- transformer_from_scratch/layers/scaled_dot_product_attention.py
+++ transformer_from_scratch/layers/scaled_dot_product_attention.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import math
 
@@ -6,33 +7,32 @@ class ScaledDotProductAttention(nn.Module):
     """
     compute scaled dot-product attention
 
-    Query : given sentence that we focused on (decoder)
-    Key : every sentence to check relationship with Qeury(encoder)
-    Value : every sentence same with Key (encoder)
+    Returns:
+        attention: multi-head attention values (batch_size, n_heads, seq_len, d_head)
+        attention_probs: attention probabilities matrix (batch_size, n_heads, seq_len, seq_len)
     """
 
     def __init__(self):
         super(ScaledDotProductAttention, self).__init__()
-        self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, q, k, v, mask=None):
-        # input is 4 dimension tensor [batch_size, n_heads, seq_len, d_head]
-        batch_size, n_heads, seq_len, d_head = k.size()
+    def forward(self, Q, K, V, mask=None):
+        # input is 4 dimension tensor (batch_size, n_heads, seq_len, d_head)
+        batch_size, n_heads, seq_len, d_head = K.size()
 
-        # 1. dot product Query with Key^T to compute similarity
-        k_t = k.transpose(2, 3)  # [batch_size, n_heads, d_head, seq_len]
-        # q @ k_t -> [batch_size, n_heads, seq_len, seq_len]
-        score = (q @ k_t) / math.sqrt(d_head)  # scaled dot-product
+        # 1. dot product Q with K^{T} to compute similarity
+        K_t = K.transpose(2, 3)  # (batch_size, n_heads, d_head, seq_len)
+        # Q @ K_t -> (batch_size, n_heads, seq_len, seq_len)
+        scaled_dot_prod = (Q @ K_t) / math.sqrt(d_head)  # scaled dot-product
 
         # 2. apply masking (opt)
         if mask is not None:
-            score = score.masked_fill(mask == 0, float('-1e20'))
+            scaled_dot_prod = scaled_dot_prod.masked_fill(mask == 0, -1e10)
 
-        # 3. pass them softmax to make [0, 1] range
-        score = self.softmax(score)
+        # 3. pass them softmax to make (0, 1) range
+        attention_probs = torch.softmax(scaled_dot_prod, dim=-1)
 
         # 4. multiply with Value
-        # score @ v -> [batch_size, n_heads, seq_len, d_head]
-        v = score @ v
+        # attention_probs @ V -> (batch_size, n_heads, seq_len, d_head)
+        attention = attention_probs @ V
 
-        return v, score
+        return attention, attention_probs
