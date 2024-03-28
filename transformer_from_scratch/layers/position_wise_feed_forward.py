d8942befed71ba64c70f6c28053a970d1e809259
Wood.FY <zfyfishwood@gmail.com>
2024-03-28 23:41:04 +0800

    Update position-wise feed-forward

--- transformer_from_scratch/layers/position_wise_feed_forward.py
+++ transformer_from_scratch/layers/position_wise_feed_forward.py
@@ -3,14 +3,20 @@ import torch.nn as nn
 
 class PositionWiseFeedForward(nn.Module):
 
-    def __init__(self, d_model, hidden, dropout=0.1):
+    def __init__(self, d_model, d_ffn, dropout=0.1):
+        """
+        Args:
+            d_model: dimension of embeddings
+            d_ffn: dimension fo feed-forward network
+            dropout: probability of dropout occurring
+        """
         super(PositionWiseFeedForward, self).__init__()
 
         self.feed_forward = nn.Sequential(
-            nn.Linear(d_model, hidden),
+            nn.Linear(d_model, d_ffn),
             nn.ReLU(),
             nn.Dropout(p=dropout),
-            nn.Linear(hidden, d_model)
+            nn.Linear(d_ffn, d_model)
         )
 
     def forward(self, x):
