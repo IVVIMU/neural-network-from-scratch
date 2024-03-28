0091dc886396146e9b97babc773414db1aa15014
Wood.FY <zfyfishwood@gmail.com>
2024-03-28 23:10:48 +0800

    Update positional encoding

--- transformer_from_scratch/embedding/positional_encoding.py
+++ transformer_from_scratch/embedding/positional_encoding.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 
 
 class PositionalEncoding(nn.Module):
@@ -7,32 +8,43 @@ class PositionalEncoding(nn.Module):
     compute positional encoding
     """
 
-    def __init__(self, d_model, max_seq_len, device):
+    def __init__(self, d_model, max_seq_len=5000, dropout=0.1):
         """
-        :param d_model: dimension of model
-        :param max_seq_len: max sequence length
-        :param device: hardware device setting
+        Args:
+            d_model: dimension of embeddings
+            max_seq_len: max sequence length
+            dropout: randomly zeroes-out some of the input
         """
         super(PositionalEncoding, self).__init__()
 
+        self.dropout = nn.Dropout(p=dropout)
+
         # same size with input matrix (for adding with input matrix)
-        self.positional_encoding = torch.zeros(max_seq_len, d_model, device=device)
-        self.positional_encoding.requires_grad = False # don't need to compute gradient
+        positional_encoding = torch.zeros(max_seq_len, d_model)
 
-        pos = torch.arange(0, max_seq_len, device=device)
+        pos = torch.arange(0, max_seq_len)
         # 1D -> 2D unsqueeze to represent word's position
-        pos = pos.float().unsqueeze(dim=1) # pos.shape: [seq_len] -> [seq_len, 1]
+        pos = pos.unsqueeze(1) # pos.shape: [seq_len] -> [seq_len, 1]
 
-        _2i = torch.arange(0, d_model, step=2, device=device).float()
+        _2i = torch.arange(0, d_model, 2)
         # compute positional encoding to consider positional information of words
-        self.positional_encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
-        self.positional_encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+        positional_encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        positional_encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+
+        # add dimension
+        positional_encoding = positional_encoding.unsqueeze(0)
+
+        # buffers are saved in state_dict but not trained by optimizer
+        self.register_buffer('positional_encoding', positional_encoding)
 
     def forward(self, x):
-        # self.positional_encoding
-        # [max_seq_len=512, d_model=512]
+        """
+        Args:
+            x: embeddings (batch_size, seq_len, d_model)
+        
+        Returns:
+            embeddings + positional encodings (batch_size, seq_len, d_model)
+        """
+        x = x + self.positional_encoding[:, : x.size(1)].requires_grad_(False)  # # don't need to compute gradient
 
-        # [batch_size=128, seq_len=30]
-        batch_size, seq_len = x.size()
-
-        return self.positional_encoding[:seq_len, :] # will add with token_embedding
+        return self.dropout(x)
